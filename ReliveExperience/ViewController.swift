f76f436b3cc6043a43d2429d0ec62c08ecb3e257
Kranthi Kumar Gurrala <Jo@kranthis-macbook-pro.local>
2015-12-08 00:47:03 -0800

    Initial Commit

---
+++ ReliveExperience/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReliveExperience
+//
+//  Created by Kranthi Kumar Gurrala on 12/8/15.
+//  Copyright © 2015 Kranthi Kumar Gurrala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
